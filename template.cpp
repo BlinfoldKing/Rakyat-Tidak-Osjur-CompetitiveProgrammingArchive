654339723ad04a0e834c8fffdf9fb723391ac706
Ganesha Danu <ganeshadanu1@gmail.com>
2018-04-24 23:53:23 +0700

    add more macro

--- template.cpp
+++ template.cpp
@@ -2,17 +2,35 @@
 
 using namespace std;
 
+#define scanI(x) scanf("%d", &x)
+#define scanC(x) scanf("%c", &x)
+#define readln(x) getline(cin, x)
+#define read(x) cin >> x
 #define Repeat(i, a, b) for(int i = a; i < b; i++)
+#define ForEach(it, obj) for(auto it = obj.begin(); it != obj.end(); i++)
+#define Within(X, A, B) (A >= X && X <= B)
+#define PB push_back()
+#define write(s) cout << s
+#define writeln(s) cout << s << '\n'
+#define len(v) v.length()
+
 typedef vector<int> vi;
 typedef map<string, int> mapSI; 
-
+typedef map<int, int> mapII; 
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int  uint64;
 
 
 int main () {
 
-    // Repeat(i, 0, 10) {
-    //     cout << "hello world";
-    // }
+    string x;
+    readln(x);
+
+    Repeat(i, 0, 10) { 
+        writeln("hello " << x << " " << len(x) << " " << i);
+    }
 
     return 0;
 
